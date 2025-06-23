8310342a2718e6fe0f3c14da709232822e333bc9
aiaja <aisyafirst@gmail.com>
2025-06-23 10:19:12 +0700

    add: profile features and api config

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -28,16 +28,25 @@
       <?php
       }
       ?>
+
       <li class="nav-item">
-        <a class="nav-link <?php echo (uri_string() == 'contact') ? "" : "collapsed" ?>" href="contact">
-          <i class="bi bi-person-rolodex"></i>
-          <span>Contact</span>
-        </a>
-      </li><!-- End Dashboard Nav -->
+          <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="profile">
+              <i class="bi bi-person"></i>
+              <span>Profile</span>
+          </a>
+      </li><!-- End Profile Nav -->
+
       <li class="nav-item">
         <a class="nav-link <?php echo (uri_string() == 'faq') ? "" : "collapsed" ?>" href="faq">
           <i class="bi bi-question-circle"></i>
           <span>F.A.Q</span>
+        </a>
+      </li>
+      <!-- End Dashboard Nav -->
+      <li class="nav-item">
+        <a class="nav-link <?php echo (uri_string() == 'contact') ? "" : "collapsed" ?>" href="contact">
+          <i class="bi bi-person-rolodex"></i>
+          <span>Contact</span>
         </a>
       </li><!-- End Dashboard Nav -->
     </ul>
