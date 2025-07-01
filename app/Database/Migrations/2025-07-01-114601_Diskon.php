6e74b89cad8f543da1ce210d73061cf35f7151d4
aiaja <aisyafirst@gmail.com>
2025-07-01 19:08:29 +0700

    add: diskon database

---
+++ app/Database/Migrations/2025-07-01-114601_Diskon.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Diskon extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'tanggal' => [
+                'type'       => 'DATE',
+                'null'       => true,
+            ],
+            'nominal' => [
+                'type' => 'DOUBLE',
+                'null' => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('diskon');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('diskon');
+    }
+}
